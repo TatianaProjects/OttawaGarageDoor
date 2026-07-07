6766b25ebf62499b354b1963c1ad9283d2ea8ffa
Tatiana <efimova.tatalex@gmail.com>
2026-07-07 11:35:43 -0400

    upadte form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,24 +10,52 @@ function Contact() {
       <section className="contact-page">
         <h1>Contact Us</h1>
 
-        <form className="contact-form">
+        <form className="contact-form"
+              action="https://formsubmit.co/efimova.tatalex@gmail.com"
+              method="POST">
+
+          <input
+            type="hidden"
+            name="_subject"
+            value="New Garage Door Request"
+          />
+
+          <input
+            type="hidden"
+            name="_captcha"
+            value="false"
+          />
+
+          <input
+            type="hidden"
+            name="_template"
+            value="table"
+          />
           <input
             type="text"
+            name="Name"
             placeholder="Your Name"
+            required
           />
 
           <input
             type="email"
+            name="Email"
             placeholder="Your Email"
+            required
           />
 
           <input
             type="tel"
+            name="Phone"
             placeholder="Phone Number"
+            required
           />
 
           <textarea
+            name="Message"
             placeholder="Tell us about your garage door issue..."
+            required
           ></textarea>
 
           <button type="submit">
